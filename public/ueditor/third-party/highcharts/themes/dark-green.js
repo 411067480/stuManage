593fdcf32289415e260c8878144f8281632f73dd
李圣威 <42173777+411067480@users.noreply.github.com>
2019-04-08 20:26:21 +0800

    Add files via upload

--- public/ueditor/third-party/highcharts/themes/dark-green.js
+++ public/ueditor/third-party/highcharts/themes/dark-green.js
@@ -8,9 +8,9 @@ Highcharts.theme = {
 		"#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
 	chart: {
 		backgroundColor: {
-			linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+			linearGradient: [0, 0, 250, 500],
 			stops: [
-				[0, 'rgb(48, 48, 96)'],
+				[0, 'rgb(48, 96, 48)'],
 				[1, 'rgb(0, 0, 0)']
 			]
 		},
@@ -129,6 +129,7 @@ Highcharts.theme = {
 			color: '#CCC'
 		}
 	},
+
 
 	navigation: {
 		buttonOptions: {
